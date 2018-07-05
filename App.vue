6b749dc3349dad9ee04696f7ce4e68f67be9c855
Viviane Araujo <viviane.baraujo1997@gmail.com>
2018-07-05 16:15:51 -0300

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+   <b-navbar toggleable="md" type="dark" variant="info">
+
+  <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+  <b-navbar-brand href="#">iObras</b-navbar-brand>
+
+  <b-collapse is-nav id="nav_collapse">
+
+
+    <!-- Right aligned nav items -->
+    <b-navbar-nav class="ml-auto">
+       <b-nav-item href="#">Login</b-nav-item>
+      <b-nav-item href="#" >Cadastre-se</b-nav-item>
+    </b-navbar-nav>
+
+  </b-collapse>
+</b-navbar>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 5px;
+}
+</style>
