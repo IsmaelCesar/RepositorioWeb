f9aa54be7cc4f63d7b5914f54de0996a2d028bb6
Viviane Araujo <viviane.baraujo1997@gmail.com>
2018-07-17 06:13:19 -0300

    cadastro andar funcionando

--- project/src/components/Cadastro_Andar.vue
+++ project/src/components/Cadastro_Andar.vue
@@ -262,6 +262,13 @@
 <script>
 export default {
   name: 'cadastroAndar',
+   created () {
+    this.$http.get('http://localhost:3000/edficios').then(response => {
+      this.edificios = response.body
+    }, response => {
+      // error callback
+    })
+  },
   data () {
 
     return {
@@ -284,7 +291,8 @@ export default {
         TubHidra:'',
         PontHidra:'',
         checked: [],
-        user:'Engenheiro'
+        user:'Engenheiro',
+        andares: []
       },
         edificios: [
         { text: 'Select One', value: null },
@@ -307,6 +315,57 @@ export default {
     }
     
 
+  },
+   methods:{
+     onSubmit() {
+      this.$http.get('http://localhost:3000/andares').then(response => {
+      this.form.andares = response.body
+    }, response => {
+      // error callback
+    })
+var quant= this.form.andares.length;
+    
+  
+    this.$http.post('http://localhost:3000/andares',{
+      id:quant,
+      nome_obra: this.form.obra,
+      nome_edf: this.form.name,
+        alvenaria: {
+        pilares: 0,
+        pilares_max:this.form.pilares,
+        vigas: 0,
+        vigas_max: this.form.vigas,
+        alvenaria: 0,
+        alvenaria_max: this.form.alvenaria
+      },
+      eletrica: {
+        tubulacoes_eletricas: 0,
+        tubulacoes_eletricas_max: this.form.TubEletrica,
+        pontos_eletricos: 0,
+        pontos_eletricos_max:this.form.PontEletrica 
+      },
+      hidraulica: {
+        tubulacoes_hidraulicas: 0,
+        tubulacoes_hidraulicas_max: this.form.TubHidra,
+        pontos_hidraulicos: 0,
+        pontos_eletricos_max: this.form.PontEletrica
+      },
+      revestimento: {
+        piso: 0,
+        piso_max: this.form.piso,
+        pintura: 0,
+        pintura_max: this.form.pintura ,
+        esquadrias: 0,
+        esquadrias_max: this.form.esquadria
+      }
+     
+      }).then(response => {
+      post.save();
+    }, response => {
+      // error callback
+    })
+
+}
   }
 }
 </script>
