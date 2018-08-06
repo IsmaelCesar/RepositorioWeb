59a17fc339d7636c3ca70f90b6fc9a5c359fda20
IsmaelCesar <leamscesar@gmail.com>
2018-08-06 20:00:56 -0300

    Comitando arquivo index modificado

--- project/src/router/index.js
+++ project/src/router/index.js
@@ -7,9 +7,6 @@ import RedefinirSenha from '@/components/RedefinirSenha'
 import User from '@/components/User'
 
 Vue.use(Router)
-Vue.use(BoostrapVue)
-Vue.use(JQuery)
-Vue.use(JQueryCycle)
 
 export default new Router({
   routes: [
