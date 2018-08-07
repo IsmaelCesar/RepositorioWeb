489a1d94ca30a274d0002195306d42a356005a68
unknown <leamscesar@gmail.com>
2018-08-07 15:08:45 -0300

    Removendo backend novamente

--- project/iObras/src/main/java/com/example/demo/repository/EdificioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.Edificio;
-
-public interface EdificioRepository extends JpaRepository<Edificio,Long>{
-
-}
--- project/iObras/src/main/java/com/example/demo/repository/ObrasRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.Obra;
-
-public interface ObrasRepository  extends JpaRepository<Obra, Long>{
-
-}
