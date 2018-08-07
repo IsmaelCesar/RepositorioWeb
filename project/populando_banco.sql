e3e8f9c4b9afa6348f1b9c2b27978e600df2acc0
unknown <leamscesar@gmail.com>
2018-08-07 15:47:20 -0300

    Adicionando arquivo para efetuar popular o banco

---
+++ project/populando_banco.sql
@@ -0,0 +1,25 @@
+
+
+insert into user values
+('Ismael Cesar','email@email.com','123','Uma casa','3333-3333','123',false),
+('Viviane Araujo','email@email.com','123','Uma casa','3333-3333','123',true);
+
+
+insert into obra values
+('Obra 1','em progresso',true,null,'313','Dolor sit amet'),
+('Obra 2','em progresso',true,null,'313','Novus ordo seculorum'),
+('Obra 3','em progresso',true,null,'313','Inquai forbutum'),
+('Obra 4','em progresso',true,null,'313','Iniqua veritatis'),
+('Obra 5','em progresso',true,null,'313','Quae tamem'),
+('Obra 6','em progresso',true,null,'313','Brutus valerius');
+
+
+
+insert into alvenaria values
+(1,10,2,15,5,30,3),
+(2,10,2,15,5,30,3),
+(3,10,2,15,5,30,3),
+(4,10,2,15,5,30,3),
+(5,10,2,15,5,30,3),
+(6,10,2,15,5,30,3),
+(7,10,2,15,5,30,3);
